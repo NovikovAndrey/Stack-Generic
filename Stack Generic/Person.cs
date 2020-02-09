ea606540ef4090dc0c174eda025b9fbc425908cc
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-09 16:18:19 +0300

    Add Person and End

---
+++ Stack Generic/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace Stack_Generic
+{
+    internal class Person
+    {
+        public string Name { get; internal set; }
+    }
+}
--- Stack Generic/Program.cs
+++ Stack Generic/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Separator;
 using System;
+using System.Collections.Generic;
 
 namespace Stack_Generic
 {
@@ -11,7 +12,34 @@ namespace Stack_Generic
             SeparateEvent separateEvent = new SeparateEvent();
             separateEvent.OnSeparate += consoleSeparator.Separator;
 
+            Stack<int> numbers = new Stack<int>();
+            numbers.Push(3);
+            numbers.Push(5);
+            numbers.Push(8);
+            foreach(int p in numbers)
+            {
+                Console.WriteLine(p);
+            }
+            separateEvent.Display();
+            var stackElement = numbers.Pop();
+            Console.WriteLine(stackElement);
+            separateEvent.Display();
 
+            Stack<Person> people = new Stack<Person>();
+            people.Push(new Person() {Name = "Tom" });
+            people.Push(new Person() { Name = "Bill" });
+            people.Push(new Person() { Name = "John" });
+            foreach (Person p in people)
+            {
+                Console.WriteLine(p.Name);
+            }
+            separateEvent.Display();
+            var person = people.Pop();
+            foreach (Person p in people)
+            {
+                Console.WriteLine(p.Name);
+            }
+            Console.ReadKey();
         }
     }
 }
